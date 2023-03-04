c3cd1c983aa544e1ca235af06592e198b315c566
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-04 15:39:48 +0100

    backend initialized

---
+++ server/database.js
@@ -0,0 +1,19 @@
+// connection to database
+
+import mongoose from 'mongoose';
+
+const connectToDatabase = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+export default connectToDatabase;
---
+++ server/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+import connectToDatabase from './database.js';
+import express from 'express';
+
+dotenv.config();
+connectToDatabase();
+const app = express();
+
+app.use(express.json());
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server runs on Port ${port}.`);
+})
