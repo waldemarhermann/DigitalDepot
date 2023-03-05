1d15f1c4dbf3ed7699c49a1e5b3b9442cd24d6ba
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-05 18:41:51 +0100

    created productRoutes and import in index.js

--- server/index.js
+++ server/index.js
@@ -3,15 +3,19 @@ import dotenv from 'dotenv';
 import connectToDatabase from './database.js';
 import express from 'express';
 
+//Routes
+import productRoutes from './routes/productRoutes.js';
+
 dotenv.config();
 connectToDatabase();
 const app = express();
 
 app.use(express.json());
 
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {
-    console.log(`Server runs on Port ${port}.`);
-})
+  console.log(`Server runs on Port ${port}.`);
+});
